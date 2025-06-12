f290762f6c198359c1b7c64e0a05af87b6aebecb
tommaso-mandorino <tommasomandorino@outlook.it>
2025-06-12 07:34:59 +0200

    feat: add card loading via js

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,3 +42,66 @@
 // #endregion Team members object array
 
 
+
+// #region Functions
+
+// Load cards with specified parameter data
+function loadCards(cardContainerSelector, cardData) {
+
+  // Get card container DOM element
+  const cardContainerElement = document.querySelector(cardContainerSelector);
+
+  // Loop for each item in card data
+  for ( let i = 0; i < cardData.length; i++ ) {
+
+    cardContainerElement.innerHTML +=
+    `
+    
+    <!-- #region Card -->
+    <div class="col-4">
+
+        <!-- #region Card row -->
+        <div class="row g-0">
+
+            <!-- #region Card image column -->
+            <div class="col-4">
+                
+                <img class="img-fluid" src="./assets/${teamMembers[i].img}" alt="Foto di ${teamMembers[i].name}">
+                
+            </div>
+            <!-- #endregion Card image column -->
+            
+            <!-- #region Card text column -->
+            <div class="col-8 p-3 bg-black text-white">
+                
+                <h5 class="fw-bold">${teamMembers[i].name}</h5>
+                
+                <div class="pb-2">${teamMembers[i].role}</div>
+                
+                <a class="text-decoration-none link-info" href="mailto:${teamMembers[i].email}">${teamMembers[i].email}</a>
+                
+            </div>
+            <!-- #endregion Card text column -->
+
+        </div>
+        <!-- #endregion Card row -->
+        
+    </div>
+    <!-- #endregion Card -->
+    
+    `;
+
+  }
+
+}
+
+// #endregion Functions
+
+
+
+// #region Script logic
+
+  // Load cards when document completely loaded
+  document.addEventListener('DOMContentLoaded', loadCards('#cards-container', teamMembers));
+
+// #endregion Script logic
